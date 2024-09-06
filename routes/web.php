9adb118e6c7a01d232e84ae477b81a76e9c08f2b
zidaneelfasya <elfasyazidan1@gmail.com>
2024-09-06 20:44:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/main', function () {
+    return view('main');
+});
+
+Route::get('/main2', function () {
+    return view('main2');
+});
